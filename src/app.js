6df9595c84f0f8ea0896c0700b4dd6edd0b18cff
egarzolegido <garzolegido@gmail.com>
2022-05-06 16:20:35 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,37 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = () => {
+  let pronoun = ["Un", "El"];
+  let subject = ["hermano", "gato", "policia", "loro", "pareja"];
+  let action = ["cogio mi", "pidio mi", "tiro mi", "robo mi", "rompio mi"];
+  let possetion = ["tarea", "zapato", "juguete", "cama"];
+  let where = ["en la calle", "en mi casa", "en el parque"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let subjectIndex = Math.floor(Math.random() * subject.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let possetionIndex = Math.floor(Math.random() * possetion.length);
+  let whereIndex = Math.floor(Math.random() * where.length);
+
+  return (
+    pronoun[pronounIndex] +
+    " " +
+    subject[subjectIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    possetion[possetionIndex] +
+    " " +
+    where[whereIndex]
+  );
 };
